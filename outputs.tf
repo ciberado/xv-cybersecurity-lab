7878b50a03b1d80234d597ed940c3f8f748084d3
Javier Moreno <email@javier-moreno.com>
2024-06-06 17:06:14 +0200

    simplificada.

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,4 @@
-# Outputs for EC2 Instances created
-output "web_app_instance_public_ip_and_name" {
-  value = {
-    ip  = aws_instance.web_app_instance.public_ip
-    tag = aws_instance.web_app_instance.tags.Name
-  }
-  description = "Web App instance and public IP"
-}
 
-# Outputs for RDS Database created
-output "database_endpoint" {
-  description = "The endpoint of the database"
-  value       = aws_db_instance.db_instance.address
-}
-
-output "database_port" {
-  description = "The port of the database"
-  value       = aws_db_instance.db_instance.port
-}
 
 # Outputs for Firewall created
 output "firewall_name" {
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   type    = string
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "cidr_block_all_traffic" {
